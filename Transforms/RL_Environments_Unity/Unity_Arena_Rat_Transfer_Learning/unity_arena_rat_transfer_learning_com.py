b0a58d27a60604d4bb04726ac1e04de76ce86502
georgedimitriadis <gdimitri@hotmail.com>
2024-12-20 14:25:22 +0000

    Unity_Arena_Rat_Transfer_Learning Node development

--- Transforms/RL_Environments_Unity/Unity_Arena_Rat_Transfer_Learning/unity_arena_rat_transfer_learning_com.py
+++ Transforms/RL_Environments_Unity/Unity_Arena_Rat_Transfer_Learning/unity_arena_rat_transfer_learning_com.py
@@ -13,19 +13,22 @@ Exec = os.path.abspath(__file__)
 
 BaseName = 'Unity Arena Rat Transfer Learning'
 NodeTooltip ='A set of Unity Games (Environments) that model a 3D rat arena\nused for a series of Transfer Learning experiments'
-NodeAttributeNames = ['Parameters', 'Message from Agent', 'Message to Agent']
-NodeAttributeType = ['Static', 'Input', 'Output']
-ParameterNames = ['Game Executable', 'Observations Returned', 'Action Space Used', 'Screen Resolution', 'Translation Snap', 'Rotation Snap']
-ParameterTypes = ['str', 'list', 'list', 'str', 'float', 'int']
-ParametersDefaultValues = ['unity_game.exe', ['Features', 'Pixels', 'Everything'], ['Small', 'Full'], '100, 100', 0.2, 10]
-ParameterTooltips = ['This is the path to the executable file that is the Unity game to be\nrun.\n',
+NodeAttributeNames = ['Parameters', 'New Environment', 'Action', 'Observations']
+NodeAttributeType = ['Static', 'Input', 'input', 'Output']
+ParameterNames = ['Visualisation', 'Path to Unity Games', 'Game Executable', 'Observations Returned', 'Action Space Used', 'Screen Resolution', 'Translation Snap', 'Rotation Snap']
+ParameterTypes = ['bool', 'str', 'str', 'list', 'list', 'str', 'float', 'int']
+ParametersDefaultValues = [False, r'path\to\Unity\build\folder', 'unity_game', ['Features', 'Pixels', 'Everything'], ['Simple', 'Full'], '100, 100', 0.2, 10]
+ParameterTooltips = ['The visualisation of the cumulative reward collected up to now',
+                     'This is the path to the the folder where the different Unity games are',
+                     'This is the name of the Unity game executable',
                      "This defines the type of observations returned by the environment  \nto the agent after the action of each step is taken. \n\nIf it is Features then the environment returns only the features of \neach state.\n\nIf it is Pixels then it returns only the array (picture) captured by the\non the angent's head.\n\nIf it is Everything then it returns both the features and the pixels.\n",
-                     'Defines if the Action space (the set of possible actions) is either\nSimple or Full.\n\nFull means all possible actions are allowed.\n\nSimple means that the Nothing action is not allowed.\n',
+                     'Defines if the Action space (the set of possible actions) is either\nSimple or Full.\n\nFull means all possible actions are allowed.\n\nSimple means that only the Move forwards, Backwards and Rotate\nLeft and Right (no Nothing and Paw Moves) are allowed.\n',
                      'The resolution of the camera that the environment uses to capture\nthe Pixels observation.\n\nThis is a comma delimited string of two numbers (X pixels, Y pixels)\nfor the horizontal and vertical dimension of the screen in pixels.\n',
                      "The discritization of the agent's translation. This is in the same units\nas the arena. The arena size in this Unity project is 8 x 8.\n\nThe value here means the size of the step the agent will take when\nmoving forwards or backwards.\n\nThe number of states in the environment is a function of this \nnumber. ",
                      "The discritization of the agent's rotation. This is in degrees.\n\nThe value here means the size of the step the agent will take when\nturning clock or counter clock wise.\n\nThe number of states in the environment is a function of this \nnumber. "]
-InOutTooltips = ['The only input to the environment comes from the agent.\nThere are three possible messages:\n\n1) A message to tell the environment to refresh. This will kill any open\nUnity games, start a new one (run the Game Executable file) and\nestablish a new communication with it.\n\n2) A message to ask the environment for the sizes of the State and\nAction spaces. This will lead to an immediate return message from \nthe environment (and this Node) describing these sizes.\n\n3) A message to ask the environment to make the agent take an \naction. The environment will enact the received action and then\nimmediately produce a message with the observation of the new \nstate.\n\n\n',
-                 'This is a reply message to the Agent (the RL model) given a previously\nreceived message.\n\nThere can be three possible responses:\n\n1) A response saying whether a reset request completed (or not)\nsuccessfully. A successful completion means a correct connection\nwith a new instance of the Unity game.\n\n2) A response describing the sizes of the Observation and Action \nspaces.\n\n3) A response with the observations after a state update.\n']
+InOutTooltips = ['np.ndarray[str] (RESET or Reset or reset)\n\nThis is a message to tell the environment to refresh. This will kill any open\nUnity games, start a new one (run the Game Executable file) and\nestablish a new communication with it.\n\n',
+                 'np.ndarray[int]\n\nThis is the input from the agent.\n\nThe environment will enact the received action and then\nimmediately produce a message with the observation of the new \nstate.\n\n',
+                 "Dict['features: np.ndarray, 'pixels': np.ndarray, 'reward': int,\n         'ter': bool, 'trunc': bool, 'actions_distribution': np.ndarray]\n\nThese are the observations of the new state after a step, together with some extra info."]
 WorkerDefaultExecutable = os.path.join(os.path.dirname(Exec), 'unity_arena_rat_transfer_learning_worker.py')
 
 
--- Transforms/RL_Environments_Unity/Unity_Arena_Rat_Transfer_Learning/unity_arena_rat_transfer_learning_worker.py
+++ Transforms/RL_Environments_Unity/Unity_Arena_Rat_Transfer_Learning/unity_arena_rat_transfer_learning_worker.py
@@ -1,7 +1,6 @@
 import sys
 from os import path
 import numpy as np
-import time
 from typing import List, Union
 
 current_dir = path.dirname(path.abspath(__file__))
@@ -12,17 +11,27 @@ sys.path.insert(0, path.dirname(current_dir))
 from Heron.communication.socket_for_serialization import Socket
 from Heron import general_utils as gu, constants as ct
 from Heron.communication.transform_worker import TransformWorker
+from Heron.gui.visualisation_dpg import VisualisationDPG
+from gymnasium_wrapper import GymEnvWrapperOfUnityGame
 
 
+visualisation_dpg: VisualisationDPG
+path_to_unity_builds: str
 game_executable: str
-observations_returned: list
-action_space_used: list
+observations_returned: str
+action_space_used: str
 screen_resolution: str
 translation_snap: float
 rotation_snap: int
+reward_history = []
+initialised = False
+gym_env: GymEnvWrapperOfUnityGame
+previous_message: str
 
 
-def initialise(worker_object: TransformWorker) -> bool:
+def get_parameters(_worker_object: TransformWorker):
+    global visualisation_dpg
+    global path_to_unity_builds
     global game_executable
     global observations_returned
     global action_space_used
@@ -30,28 +39,75 @@ def initialise(worker_object: TransformWorker) -> bool:
     global translation_snap
     global rotation_snap
 
-
     try:
-        parameters = worker_object.parameters
-        game_executable = parameters[0]
-        observations_returned = parameters[1]
-        action_space_used = parameters[2]
-        screen_resolution = parameters[3]
-        translation_snap = parameters[4]
-        rotation_snap = parameters[5]
+        parameters = _worker_object.parameters
+        path_to_unity_builds = parameters[1]
+        game_executable = parameters[2]
+        observations_returned = parameters[3]
+        action_space_used = parameters[4]
+        screen_resolution = parameters[5]
+        translation_snap = parameters[6]
+        rotation_snap = parameters[7]
     except:
         return False
 
-    # Add Saving the parameters every time they change if required:
-    #  worker_object.savenodestate_create_parameters_df(game_executable=game_executable, 
-    #                                                   observations_returned=observations_returned,
-    #                                                   action_space_used=action_space_used,
-    #                                                   screen_resolution=screen_resolution,
-    #                                                   translation_snap=translation_snap,
-    #                                                   rotation_snap=rotation_snap
-    #                                                    )
+    visualisation_dpg = VisualisationDPG(_node_name=_worker_object.node_name, _node_index=_worker_object.node_index,
+                                         _visualisation_type='Single Pane Plot', _buffer=100,
+                                         _x_axis_label='Latest Actions',
+                                         _y_axis_base_label='Cumulative Reward',
+                                         _base_plot_title='Cumulative Reward over Actions')
 
-    # DO ANY OTHER INITIALISATION HERE 
+    visualisation_dpg.visualisation_on = parameters[0]
+
+    # Add Saving the parameters every time they change if required:
+    _worker_object.savenodestate_create_parameters_df(visualisation_on=visualisation_dpg.visualisation_on,
+                                                      path_to_unity_builds=path_to_unity_builds,
+                                                      game_executable=game_executable,
+                                                      observations_returned=observations_returned,
+                                                      action_space_used=action_space_used,
+                                                      screen_resolution=screen_resolution,
+                                                      translation_snap=translation_snap,
+                                                      rotation_snap=rotation_snap)
+
+    return True
+
+
+def visualisation_reward_buffer_append(reward):
+    global reward_history
+    reward = float(reward)
+
+    if len(reward_history) > 1:
+        reward_history.append(reward_history[-1] + reward)
+    else:
+        reward_history.append(reward)
+    if len(reward_history) > 100:
+        reward_history.pop(0)
+    if visualisation_dpg.visualisation_on:
+        visualisation_dpg.visualise(np.array(reward_history))
+
+
+def initialise(_worker_object: TransformWorker) -> bool:
+    global initialised
+    global gym_env
+
+    if not get_parameters(_worker_object):
+        return False
+
+    screen_res = (int(screen_resolution.split(',')[0]), int(screen_resolution.split(',')[1]))
+
+    gym_env = GymEnvWrapperOfUnityGame(path_to_unity_builds=path_to_unity_builds,
+                                       game_executable=game_executable,
+                                       observation_type=observations_returned,
+                                       action_space_type=action_space_used,
+                                       screen_res=screen_res, move_snap=translation_snap,
+                                       rotate_snap=rotation_snap)
+
+    result_reset = gym_env.reset()
+
+    if result_reset is None:
+        return False
+
+    initialised = True
 
     return True
 
@@ -60,52 +116,73 @@ def work_function(data: List[Union[np.ndarray, dict]],
                   parameters: List,
                   savenodestate_update_substate_df: TransformWorker.savenodestate_update_substate_df) -> \
         List[Union[np.ndarray, dict]]:
-
+    global visualisation_dpg
     global game_executable
     global observations_returned
     global action_space_used
     global screen_resolution
     global translation_snap
     global rotation_snap
+    global previous_message
 
     try:
-        #  Uncomment any of the parameter updates whose values
-        #  need to update as the Graph is running.
-        # game_executable = parameters[0]
-        # observations_returned = parameters[1]
-        # action_space_used = parameters[2]
-        # screen_resolution = parameters[3]
-        # translation_snap = parameters[4]
-        # rotation_snap = parameters[5]
+        visualisation_dpg.visualisation_on = parameters[0]
+        # game_executable = parameters[1]
+        # observations_returned = parameters[2]
+        # action_space_used = parameters[3]
+        # screen_resolution = parameters[4]
+        # translation_snap = parameters[5]
+        # rotation_snap = parameters[6]
         pass
     except:
         pass
+
+    result = [np.array([ct.IGNORE])]
 
     topic = data[0]
 
     message = data[1:]
     message = Socket.reconstruct_data_from_bytes_message(message)
-    # OR Socket.reconstruct_array_from_bytes_message_cv2correction(message) if the message data is an image
 
-    # -----------------------
-    # MAIN CODE GOES HERE !!
-    # -----------------------
+    # For now set the action distribution to Uniform but eventually this will be a function of state and will be
+    # returned by the Unity Game
+    result = {'features': np.array([]), 'pixels': np.array([]), 'reward': 0, 'ter': False, 'trunc': False,
+              'actions_distribution': np.array([1 / gym_env.action_space.n] * gym_env.action_space.n)}
+
+    # If the message coming into the New Environment input asks to refresh the Unity Game,
+    # delete any running instance and start a new executable
+    if 'New Environment' in topic:
+        if message[0] == 'reset' or message[0] == 'Reset' or message[0] == 'RESET' and previous_message != message:
+            previous_message = message
+            result_reset = gym_env.reset()
+            if result_reset is not None:
+                print(result_reset)
+                obs, info = result_reset
+                result['features'] = obs['features']
+                result['pixels'] = obs['pixels']
+
+    if 'Action' in topic:
+        action = message[0]
+        #print(action)
+        obs, reward, terminated, truncated, info = gym_env.step(action)
+        visualisation_reward_buffer_append(reward)
+        result['features'] = obs['features']
+        result['pixels'] = obs['pixels']
+        result['reward'] = reward
+        result['ter'] = terminated
+        result['trunc'] = truncated
 
     # SAVE TO SUBSTATE. arguments' syntax: arg_name_in_dataframe=variable_to_save 
     #  savenodestate_update_substate_df(arg_name_in_dataframe=variable_to_save)
 
-    # Create the result to be pushed to the next Node
-    # Here we are creating a list that pushes nothing, but you should
-    # create a list of numpy arrays or dictionaries for the Node to return.
-    result = [np.array([ct.IGNORE]), ]
-
-    return result
+    return [result]
 
 
 def on_end_of_life():
-
-    # ADD HERE ANY OTHER PROCESS TERMINATION CODE
-    pass
+    try:
+        gym_env.close()
+    except:
+        pass
 
 
 if __name__ == '__main__':
